2237b180262e5e7c02024a1bf5ccea62173d131c
Zohorl <zohorul109561@gmail.com>
2022-05-08 22:27:27 +0600

    delete method error handle

--- index.js
+++ index.js
@@ -70,7 +70,9 @@ async function run() {
         // DELETE
         app.delete('/item/:id', async (req, res) => {
             const id = req.params.id;
-
+            const query = { _id: ObjectId(id) };
+            const result = await itemCollection.deleteOne(query);
+            res.send(result);
         });
     }
     finally {
