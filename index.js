d2e1eaf01be2a9054a61fa43c1d03689c6ee6b3f
Zohorl <zohorul109561@gmail.com>
2022-05-08 22:26:08 +0600

    delete method adedd

--- index.js
+++ index.js
@@ -67,6 +67,11 @@ async function run() {
             const result = await itemCollection.insertOne(newItem);
             res.send(result);
         });
+        // DELETE
+        app.delete('/item/:id', async (req, res) => {
+            const id = req.params.id;
+
+        });
     }
     finally {
 
